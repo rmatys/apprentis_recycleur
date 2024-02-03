82fa7c6d4170486ca5ad5c04bc35182d94a73c07
matrano <matysrano007@gmail.com>
2024-02-03 17:01:49 -0500

    ajout initialization

--- Apprentis_Recycleur/src/application/Panel.java
+++ Apprentis_Recycleur/src/application/Panel.java
@@ -1,10 +1,14 @@
 package application;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
 public class Panel extends JPanel {
+	private boolean premiereFois = true;
 
 	/**
 	 * Create the panel.
@@ -12,6 +16,21 @@ public class Panel extends JPanel {
 	public Panel() {
 		setLayout(null);
         setBackground(Color.gray);
+	}
+	
+	private void initialization() {
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		if (premiereFois) {
+			initialization();
+		}
 	}
 
 }
