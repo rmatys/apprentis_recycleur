50fe6ffa46f67e961aa6e851b1a8db44c7618754
matrano <matysrano007@gmail.com>
2024-02-03 17:29:44 -0500

    ajout d'image

--- Apprentis_Recycleur/src/application/Panel.java
+++ Apprentis_Recycleur/src/application/Panel.java
@@ -3,11 +3,16 @@ package application;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.RenderingHints;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel implements Runnable {
+public class Panel extends JPanel implements Runnable, Serializable {
 	private static final long serialVersionUID = -3422389399040540538L;
 	
 	private boolean premiereFois = true;
@@ -25,17 +30,24 @@ public class Panel extends JPanel implements Runnable {
 		
 	}
 	
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		if (premiereFois) {
-			initialization();
-		}
-	}
-	
+//	@Override
+//	public void paintComponent(Graphics g) {
+//		super.paintComponent(g);
+//		Graphics2D g2d = (Graphics2D) g;
+//		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//		
+//		if (premiereFois) {
+//			initialization();
+//		}
+//		
+//		Image recyclage = image("bin1.jpeg");
+//		
+//		Graphics2D g2dImage = (Graphics2D) g2d.create();
+//		g2dImage.translate(50, 50);
+//		g2dImage.drawImage(recyclage, 20, 
+//				20, null);
+//	}
+//	
 	@Override
 	public void run() {
 		
@@ -51,6 +63,18 @@ public class Panel extends JPanel implements Runnable {
 	
 	public void arreterAnim() {
 		enCoursAnim = false;
+	}
+	
+	public static Image image(String fichier) {
+		Image img = null;
+		URL urlFichier = Panel.class.getClassLoader().getResource(fichier);
+		try {
+			img = ImageIO.read(urlFichier);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return(img);
 	}
 
 }
