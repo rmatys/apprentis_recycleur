75c32ee8f1c389dbdcfd8548d8c597f7257a880c
matrano <matysrano007@gmail.com>
2024-02-03 16:41:59 -0500

    ajout constructeur poubelles et trash

--- Apprentis_Recycleur/src/objets/Poubelles.java
+++ Apprentis_Recycleur/src/objets/Poubelles.java
@@ -4,4 +4,9 @@ public class Poubelles {
 	public String nom;
 	public int idType;
 	public String image;
+	
+	public Poubelles(String nomPoubelle, int id) {
+		this.nom = nomPoubelle;
+		this.idType = id;
+	}
 }
--- Apprentis_Recycleur/src/objets/Trash.java
+++ Apprentis_Recycleur/src/objets/Trash.java
@@ -4,4 +4,9 @@ public class Trash {
 	public String nom;
 	public int idType;
 	public String image; 
+	
+	public Trash(String nomTrash, int id) {
+		this.nom = nomTrash;
+		this.idType = id;
+	}
 }
