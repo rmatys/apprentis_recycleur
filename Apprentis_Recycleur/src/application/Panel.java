a57ca7a6846fb9598066556716218dbc5ca5eaf3
matrano <matysrano007@gmail.com>
2024-02-03 15:45:54 -0500

    initial commit

---
+++ Apprentis_Recycleur/src/application/Panel.java
@@ -0,0 +1,14 @@
+package application;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel() {
+
+	}
+
+}
---
+++ Apprentis_Recycleur/src/objets/Poubelles.java
@@ -0,0 +1,5 @@
+package objets;
+
+public class Poubelles {
+
+}
