d9908c431ba424cdfaa4eb06bb426ee96d42b6fa
matrano <matysrano007@gmail.com>
2024-02-03 19:39:22 -0500

    modify dessiner

--- Apprentis_Recycleur/src/objets/Trash.java
+++ Apprentis_Recycleur/src/objets/Trash.java
@@ -1,6 +1,10 @@
 package objets;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 
 import interfaces.Dessinable;
 import interfaces.Selectionnable;
@@ -8,7 +12,12 @@ import interfaces.Selectionnable;
 public class Trash implements Selectionnable, Dessinable {
 	public String nom;
 	public int idType;
-	public String image; 
+	public String image;
+	
+	private double diametre = 10;
+	private Point2D.Double point;
+	private Ellipse2D.Double cercle= new Ellipse2D.Double();
+	
 	
 	public Trash(String nomTrash, int id) {
 		this.nom = nomTrash;
@@ -23,7 +32,16 @@ public class Trash implements Selectionnable, Dessinable {
 	
 	@Override
 	public void dessiner(Graphics2D g2d, double pixelsParMetre) {
+		Graphics2D g2dPrive = (Graphics2D) g2d.create();
 		
+		AffineTransform mat = new AffineTransform();
+		mat.scale(pixelsParMetre, -pixelsParMetre);
+		
+		g2dPrive.setColor(Color.black);
+		
+		double d = diametre / pixelsParMetre;
+		cercle = new Ellipse2D.Double(point.getX()-d/2, point.getY()-d/2, d, d);
+		g2dPrive.fill(mat.createTransformedShape(cercle));
 	}
 
 	@Override
