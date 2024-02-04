486051bd11f1decca6d2d88f738a5993f099da1a
matrano <matysrano007@gmail.com>
2024-02-03 20:14:06 -0500

    dessiner toutes les poubelles

--- Apprentis_Recycleur/src/application/System.java
+++ Apprentis_Recycleur/src/application/System.java
@@ -5,12 +5,11 @@ import java.util.ArrayList;
 import objets.Poubelles;
 
 public class System {
-	static ArrayList<Poubelles> listPoubelles = new ArrayList<Poubelles>();
 	
 	public static void ajouterPoubelles(Panel pan, int nbrPoubelles) {
 		for (int i = 0; i < nbrPoubelles; i++) {
 			Poubelles poubelle = new Poubelles("poubelle" + i, i);
-			listPoubelles.add(poubelle);
+			pan.listPoubelles.add(poubelle);
 		}
 	}
 }
