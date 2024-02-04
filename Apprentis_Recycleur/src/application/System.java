b43441509765be6d83157713784c0d0f7f03c1b3
matrano <matysrano007@gmail.com>
2024-02-03 20:05:34 -0500

    methode ajout poubelles

--- Apprentis_Recycleur/src/application/System.java
+++ Apprentis_Recycleur/src/application/System.java
@@ -1,8 +1,16 @@
 package application;
 
+import java.util.ArrayList;
+
+import objets.Poubelles;
+
 public class System {
+	static ArrayList<Poubelles> listPoubelles = new ArrayList<Poubelles>();
 	
-	public static void ajouterPoubelles() {
-		
+	public static void ajouterPoubelles(Panel pan, int nbrPoubelles) {
+		for (int i = 0; i < nbrPoubelles; i++) {
+			Poubelles poubelle = new Poubelles("poubelle" + i, i);
+			listPoubelles.add(poubelle);
+		}
 	}
 }
