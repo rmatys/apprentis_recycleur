8f3aaeb482998dda03a097cdb5f2605f8e95d2c5
TheBidus <eliott.bouteau@gmail.com>
2024-02-04 10:52:26 -0500

    Score, vies et nb restant

--- Apprentis_Recycleur/src/application/Panel.java
+++ Apprentis_Recycleur/src/application/Panel.java
@@ -157,9 +157,9 @@ public class Panel extends JPanel implements Runnable, Serializable {
 		g2d.drawString("Objet à trier: "+ trashAJeter.getNom(),getWidth()/2-127,60);
 
 		g2d.setFont(new Font("Eras Demi ITC", Font.PLAIN, 21));
-		g2d.drawString("Score : "+score, 50, 45);
-		g2d.drawString("Vies : "+ nbrVies, 50, 80);
-		g2d.drawString("Déchets restants : "+dechetsRest, 50, 115);
+		g2d.drawString("Score : "+systeme.donnees.getScore(), 50, 45);
+		g2d.drawString("Vies : "+ systeme.donnees.getVies(), 50, 80);
+		g2d.drawString("Déchets restants : "+systeme.donnees.getRestants(), 50, 115);
 	}
 
 	@Override
